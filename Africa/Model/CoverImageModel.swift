e92dccef2bdd1b27ec1bb5bc517b6ad51e12b3e9
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-14 18:57:48 -0300

    Decode local json file

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Kim Ruan on 14/09/23.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
